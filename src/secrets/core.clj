689d7a267aff0e4b1b3178cfb1e81bd80deaece6
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-09-29 15:34:43 +0300

    Reformat code

--- src/secrets/core.clj
+++ src/secrets/core.clj
@@ -8,23 +8,31 @@
   "Returns a random byte array of the specified size."
   [nbytes]
   (let [seed (byte-array nbytes)]
-    (.nextBytes (SecureRandom.) seed) seed))
+    (.nextBytes
+     (SecureRandom.) seed) seed))
 
 (defn token-bytes
   "Return a random byte string containing nbytes number of bytes. If nbytes is nil or not supplied,
   a reasonable default is used."
-  [nbytes] (get-random-bytes nbytes))
+  [nbytes]
+  (get-random-bytes nbytes))
 
 (defn token-hex
   "Return a random text string, in hexadecimal. The string has nbytes random bytes, each byte converted
   to two hex digits. If nbytes is nil or not supplied, a reasonable default is used."
-  [nbytes] (String. (Hex/encodeHex ^bytes (get-random-bytes nbytes))))
+  [nbytes]
+  (String.
+   (Hex/encodeHex ^bytes
+    (get-random-bytes nbytes))))
 
 (defn token-urlsafe
   "Return a random URL-safe text string, containing nbytes random bytes. The text is Base64 encoded,
   so on average each byte results in approximately 1.3 characters. If nbytes is nil or not supplied,
   a reasonable default is used."
-  [nbytes] (-> (String. (Base64/encodeBase64 (get-random-bytes nbytes)))
-               (st/replace "+" "-")
-               (st/replace "/" "_")
-               (st/replace "=" "")))
+  [nbytes]
+  (-> (String.
+       (Base64/encodeBase64
+        (get-random-bytes nbytes)))
+      (st/replace "+" "-")
+      (st/replace "/" "_")
+      (st/replace "=" "")))
