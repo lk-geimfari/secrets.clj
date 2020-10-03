3df2d3c8f613e3e6c40c66313d0d77efbdd3c561
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-10-03 11:16:09 +0300

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject secrets.clj "0.1.1"
+(defproject secrets "0.1.1"
   :description "A Clojure library designed to generate secure random numbers for managing secrets"
   :scm {:name "git"
         :url "https://github.com/lk-geimfari/secrets"}
