18727475b8d0d0b98a465535effd67f0ce457110
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-10-25 16:17:46 +0300

    Fix style issues in tests/

--- test/secrets/core_test.clj
+++ test/secrets/core_test.clj
@@ -1,38 +1,37 @@
 (ns secrets.core-test
-  (:require [clojure.test :refer :all]
-            [secrets.core :refer :all]))
+  (:require [clojure.test :refer [deftest, testing, is]]
+            [secrets.core :as core]))
 
 (deftest token-hex-test
   (testing "Generate a random hex string"
-    (is (= (count (token-hex)) 64))
-    (is (= (count (token-hex 64)) 128))))
+    (is (= (count (core/token-hex)) 64))
+    (is (= (count (core/token-hex 64)) 128))))
 
 (deftest token-bytes-test
   (testing "Generate the random bytes"
-    (is (= (count (token-bytes)) 32))
-    (is (= (count (token-bytes 128)) 128))))
+    (is (= (count (core/token-bytes)) 32))
+    (is (= (count (core/token-bytes 128)) 128))))
 
 (deftest token-urlsafe-test
   (testing "Generate a url-safe random string"
-    (is (= (count (token-urlsafe)) 43))
-    (is (= (count (token-urlsafe 16)) 22))))
+    (is (= (count (core/token-urlsafe)) 43))
+    (is (= (count (core/token-urlsafe 16)) 22))))
 
 (deftest uuid4-test
   (testing "Generate UUID4")
-  (is (= (count (uuid4)) 36)))
+  (is (= (count (core/uuid4)) 36)))
 
 (deftest randbelow-test
   (testing "Generate a random int in the range [0, n)")
-  (let [number (randbelow 100)]
+  (let [number (core/randbelow 100)]
     (is (and (> number 0) (< number 100)))))
 
 (deftest choice-test
   (testing "Choice a random element of coll")
-  (def numbers [8 16 32 64 128])
-  (let [chosen (choice numbers)]
+  (let [chosen (core/choice [8 16 32 64 128])]
     (is (and (>= chosen 8) (<= chosen 128)))
-    (is (thrown? java.lang.Exception (choice [])))))
+    (is (thrown? java.lang.Exception (core/choice [])))))
 
 (deftest unix-timestamp-test
   (testing "Returns UNIX-timestamp")
-  (is (instance? java.lang.Long (unix-timestamp))))
+  (is (instance? java.lang.Long (core/unix-timestamp))))
