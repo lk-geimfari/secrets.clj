501a6b96fda028ca67d572c9e933096beeeac871
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-11-08 19:37:33 +0300

    Fix style issues

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject likid_geimfari/secrets "0.2.0"
+(defproject likid_geimfari/secrets "0.2.1"
   :description "A Clojure library designed to generate secure random numbers for managing secrets"
   :scm {:name "git"
         :url "https://github.com/lk-geimfari/secrets.clj"}
--- src/secrets/core.clj
+++ src/secrets/core.clj
@@ -10,12 +10,7 @@
   "Returns a random byte array of the specified size."
   [nbytes]
   (let [bytes (byte-array nbytes)]
-    (.nextBytes (SecureRandom.) bytes) bytes))
-
-(defn uuid4
-  "Return UUID generated using a cryptographically strong pseudo random number generator."
-  []
-  (.toString (java.util.UUID/randomUUID)))
+    (.nextBytes (java.security.SecureRandom.) bytes) bytes))
 
 (defn token-bytes
   "Return a random byte string containing nbytes number of bytes. If nbytes is nil or not supplied,
@@ -23,31 +18,24 @@
   ([] (get-random-bytes 32))
   ([nbytes] (get-random-bytes nbytes)))
 
-(defn- generate-token-hex
-  [nbytes]
-  (String. (Hex/encodeHex ^bytes (get-random-bytes nbytes))))
-
 (defn token-hex
   "Return a random text string, in hexadecimal. The string has nbytes random bytes, each byte
   converted to two hex digits. If nbytes is nil or not supplied, a reasonable default is used (32)."
-  ([] (generate-token-hex 32))
-  ([nbytes] (generate-token-hex nbytes)))
-
-(defn- generate-urlsafe-token
-  [nbytes]
-  (-> (String.
-       (Base64/encodeBase64
-        (get-random-bytes nbytes)))
-      (string/replace "+" "-")
-      (string/replace "/" "_")
-      (string/replace "=" "")))
+  ([] (token-hex 32))
+  ([nbytes] (String. (Hex/encodeHex ^bytes (get-random-bytes nbytes)))))
 
 (defn token-urlsafe
   "Return a random URL-safe text string, containing nbytes random bytes. The text is Base64 encoded,
   so on average each byte results in approximately 1.3 characters. If nbytes is nil or not supplied,
   a reasonable default is used (32)"
-  ([] (generate-urlsafe-token 32))
-  ([nbytes] (generate-urlsafe-token nbytes)))
+  ([] (token-urlsafe 32))
+  ([nbytes]
+   (-> (String.
+        (Base64/encodeBase64
+         (get-random-bytes nbytes)))
+       (string/replace "+" "-")
+       (string/replace "/" "_")
+       (string/replace "=" ""))))
 
 (defn randbelow
   "Return a random int in the range [0, n)."
@@ -63,3 +51,7 @@
 (defn unix-timestamp
   "Returns a number of seconds from the Unix epoch of 1970-01-01T00:00:00Z"
   [] (.getEpochSecond (java.time.Instant/now)))
+
+(defn uuid4
+  "Return UUID generated using a cryptographically strong pseudo random number generator."
+  [] (.toString (java.util.UUID/randomUUID)))
