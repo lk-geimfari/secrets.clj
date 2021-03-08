7053e96779baf0e04f42c16e6d4cb380190edb60
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-03-08 16:09:24 +0300

    Fix strings

--- src/secrets/strings.clj
+++ src/secrets/strings.clj
@@ -1,9 +1,9 @@
 (ns secrets.strings)
 
+(def digits "0123456789")
+(def octdigits "01234567")
+(def hexdigits (str digits "abcdef" "ABCDEF"))
 (def ascii-lowercase "abcdefghijklmnopqrstuvwxyz")
 (def ascii-uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-(def ascii-letters (str ascii-lowercase ascii-uppercase))
-(def digits "0123456789")
-(def hexdigits (str digits "abcdef" "ABCDEF"))
-(def octdigits "01234567")
 (def punctuation "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+(def ascii-letters (str ascii-lowercase ascii-uppercase))
