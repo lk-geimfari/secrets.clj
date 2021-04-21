7aaebc4b0aa4e207e89736d3de3034d02ff73374
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-04-22 00:31:12 +0300

    Fix tests

--- test/secrets/core_test.clj
+++ test/secrets/core_test.clj
@@ -5,35 +5,35 @@
 
 (deftest token-hex-test
   (testing "Generate a random hex string"
-    (is (= (count (core/token-hex)) 64))
-    (is (= (count (core/token-hex 64)) 128))))
+    (assert (= (count (core/token-hex)) 64))
+    (assert (= (count (core/token-hex 64)) 128))))
 
 (deftest token-bytes-test
   (testing "Generate the random bytes"
     (false? (string/includes? (core/token-bytes) "+"))
     (false? (string/includes? (core/token-bytes) "/"))
     (false? (string/includes? (core/token-bytes) "="))
-    (is (= (count (core/token-bytes)) 32))
-    (is (= (count (core/token-bytes 128)) 128))))
+    (assert (= (count (core/token-bytes)) 32))
+    (assert (= (count (core/token-bytes 128)) 128))))
 
 (deftest token-urlsafe-test
   (testing "Generate a url-safe random string"
-    (is (= (count (core/token-urlsafe)) 43))
-    (is (= (count (core/token-urlsafe 16)) 22))))
+    (assert (= (count (core/token-urlsafe)) 43))
+    (assert (= (count (core/token-urlsafe 16)) 22))))
 
 (deftest randbelow-test
-  (testing "Generate a random int in the range [0, n)")
-  (let [number (core/randbelow 100)]
-    (is (and (> number 0) (< number 100)))))
+  (testing "Generate a random int in the range [0, n)"
+    (let [number (core/randbelow 100)]
+      (assert (and (> number 0) (< number 100))))))
 
 (deftest choice-test
-  (testing "Choice a random element of coll")
-  (let [chosen (core/choice [8 16 32 64 128])]
-    (is (and (>= chosen 8) (<= chosen 128)))
-    (is (thrown? Exception (core/choice [])))))
+  (testing "Choice a random element of coll"
+    (let [chosen (core/choice [8 16 32 64 128])]
+      (assert (and (>= chosen 8) (<= chosen 128)))
+      (is (thrown? Exception (core/choice []))))))
 
 (deftest choices-test
-  (testing "Choices a random elements of the collection")
-  (let [k 3 chosen (core/choices [8 16 32 64 128] k)]
-    (is (= () (core/choices [] 0)))
-    (is (= (count chosen) k))))
+  (testing "Choices a random elements of the collection"
+    (let [k 3 chosen (core/choices [8 16 32 64 128] k)]
+      (assert (= '() (core/choices [] 0)))
+      (assert (= (count chosen) k)))))
--- test/secrets/tools_test.clj
+++ test/secrets/tools_test.clj
@@ -1,11 +1,11 @@
 (ns secrets.tools-test
-  (:require [clojure.test :refer [deftest, testing, is]]
+  (:require [clojure.test :refer :all]
             [secrets.tools]))
 
 (deftest uuid4-test
-  (testing "Generate UUID4")
-  (uuid? (secrets.tools/uuid4)))
+  (testing "Generate UUID4"
+    (uuid? (secrets.tools/uuid4))))
 
 (deftest unix-timestamp-test
-  (testing "Returns UNIX-timestamp")
-  (is (instance? java.lang.Long (secrets.tools/unix-timestamp))))
+  (testing "Returns UNIX-timestamp"
+    (assert (instance? java.lang.Long (secrets.tools/unix-timestamp)))))
