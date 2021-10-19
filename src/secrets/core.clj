399397a8780a4298dcf87e2c7077c4385fae6270
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-10-19 23:55:01 +0300

    Fix core

--- src/secrets/core.clj
+++ src/secrets/core.clj
@@ -3,11 +3,11 @@
   suitable for managing data such as passwords, account authentication, security tokens,
   and related secrets."
   {:author "Isaak Uchakaev"
-   :last-update-date "29-03-2021"}
-  (:require [clojure.string])
+   :last-update-date "19-10-2021"}
   (:import (org.apache.commons.codec.binary Base64 Hex)
            (java.security SecureRandom)))
 
+(def ^:private default-number-of-bytes 32)
 (def ^:private secure-random
   "Creates a secure random number generator (RNG)."
   (SecureRandom.))
@@ -21,22 +21,28 @@
 (defn token-bytes
   "Return a random byte string containing nbytes number of bytes.
   If nbytes is nil or not supplied, a reasonable default is used."
-  ([] (get-random-bytes 32))
+  ([] (get-random-bytes default-number-of-bytes))
   ([nbytes] (get-random-bytes nbytes)))
 
 (defn token-hex
   "Return a random text string, in hexadecimal.
   The string has nbytes random bytes, each byte converted to two hex digits.
-  If nbytes is nil or not supplied, a reasonable default is used (32)."
-  ([] (token-hex 32))
-  ([nbytes] (Hex/encodeHexString (get-random-bytes nbytes))))
+  If nbytes is nil or not supplied, a reasonable default is used ('default-nbytes')."
+  ([] (token-hex default-number-of-bytes))
+  ([nbytes]
+   (-> nbytes
+       (get-random-bytes)
+       (Hex/encodeHexString))))
 
 (defn token-urlsafe
   "Return a random URL-safe text string, containing nbytes random bytes.
   The text is Base64 encoded, so on average each byte results in approximately 1.3 characters.
-  If nbytes is nil or not supplied, a reasonable default is used (32)."
-  ([] (token-urlsafe 32))
-  ([nbytes] (Base64/encodeBase64URLSafeString (get-random-bytes nbytes))))
+  If nbytes is nil or not supplied, a reasonable default is used ('default-nbytes')."
+  ([] (token-urlsafe default-number-of-bytes))
+  ([nbytes]
+   (-> nbytes
+       (get-random-bytes)
+       (Base64/encodeBase64URLSafeString))))
 
 (defn randbelow
   "Return a random int in the range [0, n)."
